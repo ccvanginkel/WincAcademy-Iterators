6933bf31a8a55e754661f708593ba201957aa38c
ccvanginkel <34521994+ccvanginkel@users.noreply.github.com>
2021-11-09 19:29:33 +0100

    Add files via upload

---
+++ old_style_while_loop.js
@@ -0,0 +1,14 @@
+var colors = ["yellow", "blue", "red", "orange"];
+let i = 0;
+let text = "";
+
+while (colors[i]) {
+    text += colors[i];
+    i++;
+};
+console.log(text);
+
+
+const array = ["yellow", "blue", "red", "orange"];
+
+array.forEach(element => console.log(element));
